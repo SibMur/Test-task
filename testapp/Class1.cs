34dc6f02a271e0d176280140701ff6462e957867
SibMur <antgamechanel@gmail.com>
2023-11-08 23:03:06 +0500

    Download the project

---
+++ testapp/Class1.cs
@@ -0,0 +1,18 @@
+﻿
+namespace testapp
+{
+    public static class Server
+    {
+        private static int _count = 0;
+
+        public static void AddToCount(int value)
+        {
+            Interlocked.Add(ref _count, value);           
+        }
+
+        public static int GetCount()
+        {
+            return _count;
+        }
+    }
+}
---
+++ testapp/Program.cs
@@ -0,0 +1,18 @@
+﻿
+namespace testapp
+{
+    public class Programm
+    {
+        public static void Main()
+        {
+            Parallel.For(0, 100, i =>
+            {
+                if (i == 5 || i == 98 || i == 56)
+                    Server.AddToCount(5);
+                Console.WriteLine(i + " " + Server.GetCount());
+            }            
+            
+            );
+        }
+    }
+}
